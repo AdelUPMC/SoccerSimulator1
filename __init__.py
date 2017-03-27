9b00bc2a2efa8a68a4c117a060e8ec242bff8055
EL AMRAOUI ADEL <3415266@ppti-14-308-06.ufr-info-p6.jussieu.fr>
2017-03-27 10:54:15 +0200

    maj_init

--- __init__.py
+++ __init__.py
@@ -1,11 +1,20 @@
-from soccersimulator import   SoccerTeam
-from Mon_projet import Fonceur, Attaquant, Defenseur
-
+from soccersimulator import SoccerTeam, Strategy
+import MesStrategies
+def get_team(i):
+	team = SoccerTeam(name="TheLegend27")
+	if (i==1):
+		team.add("TheLegend27",MesStrategies.Solo())
+	if (i==2):
+		team.add("TheLegend27",MesStrategies.Attaquant())	
+		team.add("O Monstro",MesStrategies.Defenseur())
+	if (i==4):
+		team.add("El Matador",MesStrategies.Defenseur())
+		team.add("O Monstro",MesStrategies.Solo())
+		team.add("LeMur",MesStrategies.Attaquant())
+		team.add("El Matador",MesStrategies.Attaquant())
+return team
 ## Creation d'une equipe
-team1 = SoccerTeam(name="team1")
-team1.add("El Matador",Fonceur()) #Strategie Fonceur
-
-team2 = SoccerTeam(name="team2")
-team2.add("El Matador",Attaquant()) #Strategie Attaquant
-team2.add("O Monstro",Defenseur()) #Strategie Defenseur
+get_team(1)
+get_team(2)
+get_team(4)
 
